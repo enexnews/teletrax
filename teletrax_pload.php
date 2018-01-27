e37c759619ab6a490b13a6e790478040b405a84b
ralph belval <ralphjoachim@live.com>
2018-01-27 17:58:59 +0100

    preps of AJAX stories load in partners section

--- teletrax_pload.php
+++ teletrax_pload.php
@@ -5,25 +5,33 @@
  * Date: 25.01.18
  * Time: 22:59
  */
+// created 25.1.2018 updated 27.1.2018 17:58
 //echo $_POST['sourceid'];
 //echo "<br>",$_POST['sdate'], " - ",$_POST['edate'];
-
-$ttx_id = $_POST['sourceid'];
-$sdate = $_POST['sdate'] ;
-$edate = $_POST['edate'] ;
-
 require('../fact15/fact_config.php');
 $CoID = new mysqli($config['dbhost'], $config['dblogin'], $config['dbpass']);
 $CoID->select_db($config['dbname']);
-//$sql ="SELECT * from partners  order by partner_code" ;
-$sql = "SELECT distinct tt_partner FROM teletrax_hits WHERE source_id = $ttx_id and (tt_detection_start >= '" . $sdate . " 00:00:00' AND tt_detection_start < '" . $edate . " 23:59:59' ) ORDER BY tt_partner asc ";
-//echo $sql;
-$result = $CoID->query($sql) ;
-echo "Item has been detected at : <br>(Date range : $sdate => $edate )";
-echo "<ol>";
-while ($row  =  $result->fetch_array()) {
-    //echo $row->partner_code,"<br>";
-    echo "<li style='color:#336699; '><strong>",$row['tt_partner'],"</strong></li>" ;
+$sdate = $_POST['sdate'] ;
+$edate = $_POST['edate'] ;
+
+if (!isset($_POST['sourceid'])) {
+    // AJAX load of Partners detected in story (STORIES section)
+    $ttx_id = $_POST['sourceid'];
+    $sql = "SELECT distinct tt_partner FROM teletrax_hits WHERE source_id = $ttx_id and (tt_detection_start >= '" . $sdate . " 00:00:00' AND tt_detection_start < '" . $edate . " 23:59:59' ) ORDER BY tt_partner asc ";
+    //echo $sql;
+    $result = $CoID->query($sql) ;
+    echo "Item has been detected at : <br>(Date range : $sdate => $edate )";
+    echo "<ol>";
+    while ($row  =  $result->fetch_array()) {
+        echo "<li style='color:#336699; '><strong>",$row['tt_partner'],"</strong></li>" ;
+    }
+    echo "</ol>";
 }
-echo "</ol>";
+else {
+    // AJAX load of stories detected at partner (PARTNERS section)
+    $ttx_id = $_POST['partnerid'];
+    echo $_POST['partnerid'];
+    echo "<br>",$_POST['sdate'], " - ",$_POST['edate'] ;
+    
+}
 $CoID->close();
