153ec130d302410659a2a366d96d2772419b8584
Ralph Joachim <ralphjoachim@outlook.com>
2019-04-12 17:51:54 +0200

    update of current years stories report page switch to proper sourced/3rdparty version

--- teletrax_lib.php
+++ teletrax_lib.php
@@ -446,7 +446,7 @@ function ttx_top_stories_month($p_date,$ttx_type,$ttx_filter,$ttx_limit) {
         <a href='index.php?tb=$pageparam&dt=", (substr(date('Y-m-d', strtotime("-1 year", strtotime($p_date))), 0, 5) . "01-01"), "' class='waves-effect waves-light btn'>", date('Y', strtotime("-1 year", strtotime($p_date))), "</a>
         <a href='index.php?tb=$pageparam&dt=", (substr(date('Y-m-d', strtotime("-0 month", strtotime($p_date))), 0, 5) . "01-01"), "' class='waves-effect waves-light btn blue'>", date('Y', strtotime("0 year", strtotime($p_date))), "</a>";
         if (substr($p_date, 0, 5)!= substr($today, 0, 5)) {
-            echo " <a href='index.php?tb=11&dt=", (substr(date('Y-m-d', strtotime("-0 month", strtotime($today))), 0, 5) . "01-01"), "' class='waves-effect waves-light btn grey'>", date('Y', strtotime("-0 month", strtotime($today))), "</a>";
+            echo " <a href='index.php?tb=$pageparam&dt=", (substr(date('Y-m-d', strtotime("-0 month", strtotime($today))), 0, 5) . "01-01"), "' class='waves-effect waves-light btn grey'>", date('Y', strtotime("-0 month", strtotime($today))), "</a>";
         }
          echo "</div>";
         $bench_start_date = substr($p_date,0,5)."01-01" ;
