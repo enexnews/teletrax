0aea6079fa651ea358207cf9bd6fcc23ccb4842c
ralphenex <ralphjoachim@outlook.com>
2019-07-05 12:59:35 +0200

    update to modal load  and selfhit <> 'YES'

--- teletrax_pload.php
+++ teletrax_pload.php
@@ -18,7 +18,7 @@ $edate = $_POST['edate'] ;
 if (isset($_POST['sourceid'])) {
     // AJAX load of Partners detected in story (STORIES section)
     $ttx_id = $_POST['sourceid'];
-    $sql = "SELECT distinct tt_partner FROM teletrax_hits WHERE source_id = $ttx_id and (tt_detection_start >= '" . $sdate . " 00:00:00' AND tt_detection_start < '" . $edate . " 23:59:59' ) ORDER BY tt_partner asc ";
+    $sql = "SELECT distinct tt_partner FROM teletrax_hits WHERE source_id = $ttx_id and (tt_detection_start >= '" . $sdate . " 00:00:00' AND tt_detection_start < '" . $edate . " 23:59:59' )  and selfhit <> 'YES' ORDER BY tt_partner asc ";
     //echo $sql;
     $result = $CoID->query($sql) ;
     echo "Item has been detected at : <br> Date range : $sdate => $edate ( end date excluded )";
@@ -31,7 +31,7 @@ if (isset($_POST['sourceid'])) {
 else {
     // AJAX load of stories detected at partner (PARTNERS section)
     $ttx_id = $_POST['partnerid'];
-    $sql = "SELECT source_title,source_partner,tt_asset,source_id, source_date FROM teletrax_hits WHERE tt_detection_start >= '".$sdate."' AND tt_detection_start < '".$edate."'   and tt_partner = '".$ttx_id."' group by tt_asset order by source_partner asc ";
+    $sql = "SELECT source_title,source_partner,tt_asset,source_id, source_date FROM teletrax_hits WHERE tt_detection_start >= '".$sdate."' AND tt_detection_start < '".$edate."'   and tt_partner = '".$ttx_id."'  and selfhit <> 'YES'  group by tt_asset order by source_partner asc ";
     //echo $sql;
     $result = $CoID->query($sql) ;
     echo "<h6> Date range : $sdate => $edate ( end date excluded ) </h6>";
